36b5525a4061f9cf4d9ba682daf2f0f8dcd884b5
stitre <58717057+stitre@users.noreply.github.com>
2023-03-22 00:59:18 +0600

    Hot Fix Update
    +GA, Improvements

--- js/custom.js
+++ js/custom.js
@@ -3,19 +3,29 @@ const sections = document.querySelectorAll('section');
 
 // Define the function to change the opacity of sections
 const changeOpacity = () => {
-sections.forEach((section) => {
+  sections.forEach((section) => {
     const sectionTop = section.offsetTop;
     const sectionHeight = section.clientHeight;
     const windowHeight = window.innerHeight;
     const scrollTop = window.pageYOffset;
 
     // Check if the section is in the viewport
-    if (scrollTop >= sectionTop - windowHeight + (sectionHeight / 2) && scrollTop < sectionTop + sectionHeight) {
-    section.classList.add('is-visible');
+    if (window.matchMedia('(max-width: 767px)').matches) {
+      // Condition for small screens
+      if (scrollTop >= sectionTop - windowHeight + (sectionHeight / 4) && scrollTop < sectionTop + sectionHeight) {
+        section.classList.add('is-visible');
+      } else {
+        section.classList.remove('is-visible');
+      }
     } else {
-    section.classList.remove('is-visible');
+      // Condition for larger screens
+      if (scrollTop >= sectionTop - windowHeight + (sectionHeight / 2) && scrollTop < sectionTop + sectionHeight) {
+        section.classList.add('is-visible');
+      } else {
+        section.classList.remove('is-visible');
+      }
     }
-});
+  });
 };
 
 // Register the scroll event listener
